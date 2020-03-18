05480980169f35edb42dc488da961a48f8b83c4b
Liberty Failor <Libertyjayne@gmail.com>
2020-03-18 13:08:40 -0500

    first commit

---
+++ UnitTest1.cs
@@ -0,0 +1,40 @@
+using NUnit.Framework;
+// using OpenQA.Selenium.Support.UI;
+// using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+// using System;
+// using System.Diagnostics;
+// using System.Threading;
+// using System.Collections.Generic;
+
+namespace Automation
+{
+    [TestFixture]
+    public class Tests
+    {
+        IWebDriver driver;
+        
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void TestSignIn()
+        {
+            driver.Url = "https://profile-ci-web.azurewebsites.net/consultants";
+            IWebElement addConsultantButton = driver.FindElement(By.ClassName("add-button"));
+            addConsultantButton.Click();
+            addConsultantButton.SendKeys("Liberty");
+            addConsultantButton.SendKeys("\t");
+        }
+
+        [OneTimeTearDown]
+        public void Close()
+        {
+            driver.Close();
+        }
+    }
+}
